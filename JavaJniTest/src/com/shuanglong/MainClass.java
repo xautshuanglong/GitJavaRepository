c02a658c4289155c45e74a2827dec084c2f4fc00
xautshuanglong <xjshuanglong@126.com>
2017-01-06 11:12:36 +0800

    initialize JavaJniTest project

---
+++ JavaJniTest/src/com/shuanglong/MainClass.java
@@ -0,0 +1,9 @@
+package com.shuanglong;
+
+public class MainClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("=========================== Java JNI Test ===========================");
+	}
+}
